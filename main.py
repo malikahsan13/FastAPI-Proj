3b0db43ac003c6c3d726c684e5f230cb14e56668
Malik Ahsan <malikahsan13@gmail.com>
2026-02-26 12:56:19 +0500

    fixed cors issues

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173/'],
+    allow_origins=['http://localhost:5173'],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
