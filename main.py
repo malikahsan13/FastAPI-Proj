0faec0d404b625a7146299e86af8748602bfce3e
Malik Ahsan <malikahsan13@gmail.com>
2026-02-26 12:54:35 +0500

    added react app and cors in main file of fast api proj

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 from database import engine
 from models import Base
 from crud import router as crud_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[]'http://localhost:5173/'],
+    allow_credentials = True,
+    allow_method = ["*"],
+    allow_headers = ["*"]
+)
 
 app.include_router(crud_router, prefix='/todo', tags=["Crud Router"])
 
